88e27a80f6f788d43e8c44ee7c0200efaf675301
Pablinho <pabloomar2002@hotmail.com>
2023-03-08 18:08:37 -0500

    UI0.2

--- lib/Screens/profile.dart
+++ lib/Screens/profile.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:insta_furbo/utils/colors.dart';
 
 class ProfileScreen extends StatelessWidget {
   @override
@@ -6,12 +8,11 @@ class ProfileScreen extends StatelessWidget {
     return DefaultTabController(
       length: 2,
       child: Scaffold(
-        body: Container(
-          child: ListView(
-            children: [
-              PerfilTop(),
-            ],
-          ),
+        body: ListView(
+          children: [
+            PerfilTop(),
+            PerfilBottom(),
+          ],
         ),
       ),
     );
@@ -19,32 +20,58 @@ class ProfileScreen extends StatelessWidget {
 }
 
 class PerfilTop extends StatelessWidget {
+  const PerfilTop({super.key});
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height / 3.5,
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+            image: AssetImage('lib/Assets/img/canchita.png'),
+            fit: BoxFit.cover),
+      ),
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height/3.5,
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage(
-                    'lib/Assets/img/cancha.png'),
-                fit: BoxFit.cover,
-              ),
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: const [
+          CircleAvatar(
+            radius: 60,
+            backgroundColor: Colors.white,
+            child: CircleAvatar(
+              radius: 55,
+              foregroundImage: NetworkImage('https://i.imgur.com/jeoaplb.jpg'),
             ),
           ),
-          TabBar(
-            tabs: [
-              Tab(
-                text: 'asd',
-              ),
-              Tab(text: 'yalabethia')
-            ],
-            labelColor: Colors.black,
-          ),
+          Text('"Nombre panacota"',
+              style: TextStyle(
+                  fontSize: 24,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold)),
+          Text('Scout',
+              style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold))
+        ],
+      ),
+    );
+  }
+}
+
+class PerfilBottom extends StatelessWidget {
+  const PerfilBottom({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const Material(
+      color: primaryColor,
+      child:  TabBar(
+        labelStyle: TextStyle(fontWeight: FontWeight.w500, fontSize: 14, color: Colors.white),
+        indicatorColor: Colors.white,
+        indicatorSize: TabBarIndicatorSize.label,
+        indicatorWeight: 4,
+        tabs: [
+          Tab(text: 'Informacion Deportiva'),
+          Tab(text: 'Media')
         ],
       ),
     );
