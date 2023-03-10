2c14a99736b93561879bcd0a979f2630a42ef158
Pablinho <pabloomar2002@hotmail.com>
2023-03-10 12:59:28 -0500

    Perfil+auth

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -1,4 +1,4 @@
 import 'package:flutter/material.dart';
 
 const primaryColor = Color(0xFF318F31);
-const secundaryColor = Color.fromARGB(255, 39, 39, 39);
+const secundaryColor = Color.fromARGB(255, 255, 255, 255);
--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,4 +1,3 @@
-import 'dart:js';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:image_picker/image_picker.dart';
